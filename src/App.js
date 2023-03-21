f4019967ca37cdb99d3d66fe5badf0edad9f8efb
Banji Zillight <banjiolawolez@gmail.com>
2023-03-21 12:59:42 +0100

    troubleshooting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-//import React, { Component } from 'react';
+import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
+//import Welcome from './components/Welcome';
 
-function App (){
-  return (
-    <div className="App">
-      <Greet/>
-    </div>
-  );
-    
+class App extends Component {
+  render (){
+    return (
+      <div className="App">
+        <Greet/>
+        <welcome />
+      </div>
+    );
+  }     
 }
 
 export default App;
