ed7fff07e74902774f253f3224ef539e290a2cba
Banji Zillight <banjiolawolez@gmail.com>
2023-03-21 12:19:53 +0100

    computing greet component using arrow function

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-function Greet (){
-    return <h1>Hey React World! It's Zillight!</h1>
-}
+const Greet = () => <h1>Hey React World! It's Zillight!</h1>
+
 
 export default Greet;
